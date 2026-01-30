901bac127db80edc66a885f5bbd883bc653b3499
devluizaugusto <dev.luiz.augusto@gmail.com>
2026-01-30 10:00:01 -0300

    Reconfiguração da porta do servidor

--- frontend/vite.config.js
+++ frontend/vite.config.js
@@ -8,7 +8,7 @@ export default defineConfig({
     port: 4175,
     proxy: {
       '/api': {
-        target: 'http://localhost:3001',
+        target: 'http://0.0.0.0:3301',
         changeOrigin: true,
         secure: false,
         rewrite: (path) => path
